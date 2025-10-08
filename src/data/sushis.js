e86c30bc6f682339860367a037973d0ea613a6fa
Nathaly <nathaly.jp03@gmail.com>
2025-10-07 22:38:21 -0300

    :construction: adicionado fake API

---
+++ src/data/sushis.js
@@ -0,0 +1,87 @@
+let sushis = [
+  { 
+    id: 1,
+    imagem: "img",
+    nome: "Temaki Ebi", 
+    descricao: "Cone de alga nori recheado com arroz e camarão." 
+  },
+  { 
+    id: 2,
+    imagem: "img",
+    nome: "Temaki Atum", 
+    descricao: "Cone de alga nori recheado com arroz e atum." 
+  },
+  { 
+    id: 3,
+    imagem: "img",
+    nome: "Temaki Salmão", 
+    descricao: "Cone de alga nori recheado com arroz e salmão."
+  },
+  { 
+    id: 4,
+    imagem: "img",
+    nome: "Temaki Sashimi", 
+    descricao: "Cone de alga nori recheado com arroz e fatias finas de peixe cru." 
+  },
+  { 
+    id: 5,
+    imagem: "img",
+    nome: "Temaki Skin", 
+    descricao: "Cone de alga nori recheado com arroz e pele de salmão crocante."
+  },
+  { 
+    id: 6,
+    imagem: "img",
+    nome: "Temaki Philadelphia", 
+    descricao: "Cone de alga nori recheado com arroz e cream cheese." 
+  },
+  { 
+    id: 7,
+    imagem: "img",
+    nome: "Sashimi de Atum", 
+    descricao: "Fatias finas de Atum cru." 
+  },
+  { 
+    id: 8,
+    imagem: "img",
+    nome: "Sashimi de Salmão", 
+    descricao: "Fatias finas de Salmão cru." 
+  },
+  { 
+    id: 9,
+    imagem: "img",
+    nome: "Sashimi de Linguado", 
+    descricao: "Fatias finas de Linguado cru." 
+  },
+  { 
+    id: 10,
+    imagem: "img",
+    nome: "Gunkan Maki", 
+    descricao: "Arroz envolto por alga nori, coberto com ingredientes como ovas de peixe." 
+  },
+  { 
+    id: 11,
+    imagem: "img",
+    nome: "Hot Rolls", 
+    descricao: "Rolinhos de sushi empanados e fritos, com recheio quente por dentro."
+  },
+  { 
+    id: 12,
+    imagem: "img",
+    nome: "Sushi Joe", 
+    descricao: "Bolinho de arroz envolto por peixe e cream cheese, sem alga nori."
+  },
+  { 
+    id: 13,
+    imagem: "img",
+    nome: "Oshizushi", 
+    descricao: "Sushi prensado, onde o arroz e os ingredientes são pressionados em uma forma de madeira."
+  },
+  { 
+    id: 14,
+    imagem: "img",
+    nome: "Narezushi", 
+    descricao: "Peixe fermentado por vários meses, resultando em um sabor forte e distinto." 
+  }
+];
+
